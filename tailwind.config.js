8cdd754c5e87c1a05dffce95e4b606da47a28840
lluciien <qq5546999@163.com>
2024-12-24 01:54:19 +0800

    Update project dependencies and configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  darkMode: ['class', '[data-theme="dark"]'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    darkMode: ['class', '[data-theme="dark"]'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
