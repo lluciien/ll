7f9c60b1ac46fdbb998ab2feae062228ee6efb9d
luccien <66145711+lluciien@users.noreply.github.com>
2024-12-23 23:39:21 +0800

    Delete forever-love-xx/forever-love-xx directory

--- forever-love-xx/forever-love-xx/components/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes'
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider attribute="data-theme" defaultTheme="system" enableSystem {...props}>{children}</NextThemesProvider>
-}
-
